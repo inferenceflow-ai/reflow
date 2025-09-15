b398b84abafb73dd409b36e3dcc02dcbb731f5f3
Randy May <randy@mathysphere.com>
2025-09-14 21:03:14 -0400

    example 1 basically working

--- examples/01_temp_monitor_flow.py
+++ examples/01_temp_monitor_flow.py
@@ -1,14 +1,13 @@
 import asyncio
 import logging
 import random
-from multiprocessing import Process
+import sys
 from typing import List
 
-from reflow.flow_engine import FlowEngine
-from reflow.cluster import FlowCluster
 from reflow import EventSource, EventSink, EventTransformer
+from reflow.cluster import FlowCluster
 from reflow.typedefs import EndOfStreamException
-import reflow.internal.network as network
+
 
 class RandomWalkTempSimulator:
     """
@@ -33,6 +32,7 @@ class RandomWalkTempSimulator:
     def next(self, limit: int) -> List[float]:
         num_to_produce = min(limit, self.max_count - self.count) if self.max_count > 0 else limit
         if num_to_produce == 0:
+            logging.info("END OF STREAM")
             raise EndOfStreamException
 
         result = []
@@ -55,45 +55,29 @@ def check_temp(temp: float):
 
 def print_sink(events: List[str]) -> int:
     for event in events:
-        print(f'EVENT: {event}')
+        logging.info(f'EVENT: {event}')
 
     return len(events)
 
-def engine_runner(cluster_number: int, cluster_size: int, port: int):
-    logging.basicConfig(level=logging.INFO)
-    asyncio.run(run_engine(cluster_number, cluster_size, port))
 
-async def run_engine(cluster_number: int, cluster_size: int, port: int):
-    with FlowEngine(cluster_number=cluster_number,
-                    cluster_size=cluster_size,
-                    default_queue_size=100,
-                    port=port) as engine:
-        await engine.run()
-
-def init_fn_factory():
-    source_class = RandomWalkTempSimulator
-    return lambda: source_class(98, [-1.0, 0, 1.0], 100)
-
-
-async def main():
-    source = EventSource(init_fn_factory()).with_producer_fn(RandomWalkTempSimulator.next)
+def create_flow()->EventSource:
+    source = EventSource(lambda: RandomWalkTempSimulator(98, [-2.0, -1.0, 0.0, 1.0, 2.0], 100)).with_producer_fn(RandomWalkTempSimulator.next)
     temp_filter = EventTransformer(expansion_factor=1.0).with_transform_fn(check_temp)
     sink = EventSink().with_consumer_fn(print_sink)
-    source.send_to(temp_filter).send_to(sink)
+    source.send_to(sink)
+    return source
 
-    flow_cluster = FlowCluster([network.ipc_address_for_port(5001)])
-    job_id = await flow_cluster.deploy(source)
-    await flow_cluster.wait_for_completion(job_id, 10)
-    await flow_cluster.request_shutdown()
+
 
 if __name__ == '__main__':
+    engine_addresses = ['tcp://localhost:5001', 'tcp://localhost:5002']
+
+    async def deploy_and_wait_for_flow(engine_addresses: List[str], flow: EventSource, timeout_secs: int):
+        flow_cluster = FlowCluster(engine_addresses)
+        job_id = await flow_cluster.deploy(flow)
+        await flow_cluster.wait_for_completion(job_id, timeout_secs)
+
+
     logging.basicConfig(level=logging.INFO)
-    # start the engine
-    engine_proc = Process(target=engine_runner, args=[0,1,5001])
-    engine_proc.start()
-
-    #deploy the job
-    asyncio.run(main())
-
-    # wait for engines to stop
-    engine_proc.join()
+    flow = create_flow()
+    asyncio.run(deploy_and_wait_for_flow(engine_addresses, flow, 10))
